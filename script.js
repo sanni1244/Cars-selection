b4360be7ae65654431cd800409e6caf2b9aaced7
Moshood O <sanniopeyemiolayinka@gmail.com>
2021-10-27 05:13:03 +0100

    Hello

--- script.js
+++ script.js
@@ -11,7 +11,7 @@
       image: "Cars/Bugatti.jpg",
       name: "Bugatti Divo",
       price: "$5.8 million",
-      details: " Engine - 8,0 l W16 <br>Power	1,479 hp @ 6,900 rpm (1,103 kW) <br>Torque	1,180 lb·ft @ 2,000 – 6,000 rpm (1,600 N·m) <br>Induction	Turbocharged",
+      details: " Engine - 8,0 l W16 <br>Power	1,479 hp @ 6,900rpm 1,103 kW<br>Torque	1,180 lb·ft @ 2,000 – 6,000 rpm (1,600 N·m) <br>Induction	Turbocharged",
       sec_link: "landing.html#inform2"
 
     },
@@ -19,7 +19,7 @@
       image: "Cars/Vodo.jpg",
       name: "Ferrari 250 SWB",
       price: "$950,000",
-      details: "Up until now, the project had been codenamed ‘Moderna’, but GTO Engineering has seen fit to ditch the pun and christen the creation Squalo because of its 'shark-like look, instincts and speed'. You’re going to need a bigger boat, etc.",
+      details: "Up until now, the project had been codenamed ‘Moderna’, but GTO Engineering has seen fit to ditch the pun and christen the creation Squalo because of its 'shark-like look, instincts and speed'.",
       sec_link: "landing.html#inform3"
 
     },
@@ -35,7 +35,7 @@
       image: "Cars/lambor.jpg",
       name: "Toyota SUV Tacoma Custom",
       price: "$30,000",
-      details: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Animi, quam dolorem voluptatum adipisci dignissimos ipsum, architecto, fugit suscipit odio itaque veritatis expedita exercitationem corporis! Est odio quo possimus corporis fuga. Animi, quam dolorem voluptatum adipisci dignissimos ipsum, architecto, fugit suscipit odio itaque veritatis expedita exercitationem corporis! Est odio quo possimus corporis fuga. ",
+      details: "Lorem ipsum dolor sit amet consectetur adipisicing architecto, fugit suscipit odio itaque veritatis expedita exercitationem corporis!",
       sec_link: "landing.html#inform5"
 
      
@@ -44,7 +44,7 @@
       image: "Cars/redcar.jpg",
       name: "Ferrari 599 gto",
       price: "$410,000",
-      details: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Animi, quam dolorem voluptatum adipisci dignissimos ipsum, architecto, fugit suscipit odio itaque veritatis expedita exercitationem corporis! Est odio quo possimus corporis fuga. Animi, quam dolorem voluptatum adipisci dignissimos ipsum, architecto, fugit suscipit odio itaque veritatis expedita exercitationem corporis! Est odio quo possimus corporis fuga. ",
+      details: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Animi, quam dolorem voluptatum adipisci dignissimos ipsum, architecto, fugit suscipit odio itaque fuga. Animi, quam dolorem voluptatum adipisci",
       sec_link: "landing.html#inform6"
 
 
@@ -62,7 +62,7 @@
       image: "Cars/2022-91000.png",
       name: "2022 Tesla Model S",
       price: "$97,000",
-      details: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Animi, quam dolorem voluptatum  odio itaque veritatis expedita exercitationem corporis! Est odio quo possimus corporis fuga. ",
+      details: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Atis expedita exercitationem corporis! Est odio quo possimus corporis fuga. ",
       sec_link: "landing.html#inform8"
 
      
@@ -71,7 +71,7 @@
       image: "Cars/chiron3.5.jpg",
       name: "Bugatti Chiron",
       price: "$3.5 million",
-      details: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Animi, quam dolorem voluptatum adipisci dignissimos ipsum, architecto, fugit suscipit odio itaque veritatis expedita exercitationem corporis! Est odio quo possimus corporis fuga. Animi, quam dolorem voluptatum adipisci dignissimos ipsum, architecto, fugit suscipit odio itaque veritatis expedita exercitationem corporis! Est odio quo possimus corporis fuga. ",
+      details: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Animi, fuga. Animi, quam dolorem voluptatum adipisci dignissimos ipsum, architecto, fugit",
       sec_link: "landing.html#inform9"
 
 
@@ -80,7 +80,7 @@
       image: "Cars/lamcar.jpg",
       name: "Lamborghini Aventador 2022",
       price: "$3.5 million",
-      details: "With a raucous 769-hp V-12 engine and an exotic exterior, the 2022 Lamborghini Aventador is the definition of a hyper car. While competitors such as the Ferrari SF90 embrace an electrified future, the Lambo's 12-cylinder mill swills fuel like a frat bro chugs beer. Its scissor doors, wide and low proportions, and heavily vented bodywork double as a theater on wheels that's perpetually playing the Fast and Furious movies.",
+      details: "With a raucous 769-hp V-12 engine and an exotic exterior, the 2022 Lamborghini Aventador is the definition of a hyper car. While competitors such as the Ferrari SF90 embrace an electrified future",
       sec_link: "landing.html#inform10"
 
     },
@@ -88,7 +88,7 @@
       image: "Cars/Aventdor36.jpg",
       name: "Lamborghini Aventador SV 2016",
       price: "$490,000",
-      details: "It packs a 6.5-liter V-12 that produces 740 hp. Power is sent to all four wheels via a seven-speed automated manual gearbox. Compared to the original Aventador, this model sheds 110 pounds thanks to the extensive use of carbon fiber. Lamborghini estimates it should hit 62 mph in 2.8 seconds, with a top speed of more than 217 mph.",
+      details: "It packs a 6.5-liter V-12 that produces 740 hp. Power is sent to all four wheels via a seven-speed automated manual gearbox.",
       sec_link: "landing.html#inform11"
 
     },
@@ -96,7 +96,7 @@
       image: "Cars/niceone.jpg",
       name: "Bugatti chiron 2021",
       price: "$2.9 million",
-      details: "It's powered by a massive 16-cylinder engine that is a marvel of modern engineering, capable of propelling this hypercar to 300 mph and beyond. Besides performance that can alter time, the Chiron looks like nothing else on the road—save for maybe a Veyron—and its interior is lined with fine materials worthy of its price tag.",
+      details: "It's powered by a massive 16-cylinder engine that is a marvel of modern engineering, capable of propelling this hypercar to 300 mph and beyond.",
       sec_link: "landing.html#inform12"
 
     }
